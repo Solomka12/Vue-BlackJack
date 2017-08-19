414f7a9242ea4ac181d40572a042d6a69b886031
Solomka <solomahinigor@gmail.com>
2017-08-20 02:33:49 +0300

    It's finaly working
    Main functions is working now. Game can be played. A lot of work to be
    done with styling.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp'),
+			autoprefixer = require('gulp-autoprefixer'),
+			cleanCSS = require('gulp-clean-css'),
+			browserSync = require('browser-sync').create();
+
+const config = {
+	src: './res',
+	css: {
+		src: '/precss/**/*.css',
+		dest: '/css'
+	}
+}
+
+gulp.task('build', function() {
+	gulp.src(config.src + config.css.src)
+			.pipe(autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			}))
+			.pipe(cleanCSS())
+			.pipe(gulp.dest(config.src + config.css.dest))
+});
+
+gulp.task('watch', ['browserSync'], function() {
+	gulp.watch(config.src + config.css.src, ['build'])
+});
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: config.src
+		}
+	});
+});
--- res/js/app.js
+++ res/js/app.js
@@ -32,8 +32,13 @@ var vm = new Vue({
 
 		player:{ 
 			hand: [],
+			cash: 1000,
 			points: 0,
+			bet: 10,
 		},
+
+		betMade: false,
+		cantTakeMore: false,
 
 		dealer: {
 			hand: [],
@@ -75,25 +80,74 @@ var vm = new Vue({
 		},
 
 		startNewHand: function () {
-			console.log('New game has been started');
+			this.cantTakeMore = false;
+			this.betMade = false;
 			this.player.hand = [];
 			this.dealer.hand = [];
-
-			this.addCardTo(this.dealer);
-			this.addCardTo(this.player);
-			this.addCardTo(this.dealer);
-			this.addCardTo(this.player);
 		},
 
-		checkBast: function(person) {
-			if (this.getPoints(person) > 21) {
-				console.log('Overfull!');
+		giveOutCards: function() {
+			this.player.cash -= this.player.bet;
+			this.betMade = true;
+			console.log('New game has been started');
+
+			this.addCardTo(this.dealer);
+			this.addCardTo(this.player);
+			this.addCardTo(this.player);
+			if (this.getPoints(this.player) === 21) this.startDealerPlay();
+		},
+
+		takeCard: function() {
+			this.addCardTo(this.player);
+			var points = this.getPoints(this.player);
+			if (points === 21) this.startDealerPlay();
+			else if (points > 21) { 
+				this.cantTakeMore = true;
+				console.warn('Going bust! You Lose...');
+				setTimeout(this.startNewHand, 2000);
 			}
 		},
 
-		showPoints: function(person) {
-			person.points = this.getPoints(person);
+		startDealerPlay: function() {
+			this.cantTakeMore = true;
+
+			/*----------Дилер добирает карты-----------*/
+			do {
+				this.addCardTo(this.dealer);
+			} while (this.getPoints(this.dealer) < 17);
+
+			this.summarizing();
 		},
+		
+		/*----------Сравниваются очки дилера и игрока-----------*/
+		summarizing: function() {
+			if (this.getPoints(this.dealer) > 21 || this.getPoints(this.player) > this.getPoints(this.dealer)) {
+				this.player.cash += this.player.bet * 2;
+				console.info('You won ' + this.player.bet * 2 + '$');
+			} else if (this.getPoints(this.player) === this.getPoints(this.dealer)) {
+				this.player.cash += +this.player.bet;
+				console.warn('Draw');
+			} else {
+				console.warn('You Lose...');
+			}
+			setTimeout(this.startNewHand, 2000);
+		},
+
+			
+
+
+			// var points = this.getPoints(this.dealer);
+			// var i = 0;
+			// do {
+			// 	setTimeout(function(that) {
+			// 		return function() {
+			// 			console.log(that);
+			// 			that.addCardTo(that.dealer);
+			// 		}; 
+			// 	}(this), 1000*i);
+			// 	points = this.getPoints(this.dealer);
+			// 	i++;
+			// }	while (points < 17);
 
 		getPoints: function(person) {
 			var ace = 0;
@@ -115,6 +169,7 @@ var vm = new Vue({
 			}, 0);
 		},
 
+
 		changeCardsMargin: function(person) {
 			var fieldWidth = document.querySelector('div.playing-field').clientWidth;
 			var cards = person.hand.length;
@@ -134,14 +189,14 @@ var vm = new Vue({
 			// if (this.getPoints(this.player) < 21 && this.getPoints(this.player) > this.getPoints(this.dealer)) console.info('You won!');
 			// else if (this.getPoints(this.player) > 21) console.warn('Overfull! You lose...');
 			// else console.warn('You lose(');
-
 			this.player.points = this.getPoints(this.player);
-			//console.log(this.player.points);
-			return this.getPoints(this.player);
+			// console.log(this.player.points);
+			return this.player.points;
 		},
 		
 		setDealerPoints: function() {
-			return this.getPoints(this.dealer);
+			this.dealer.points = this.getPoints(this.dealer);
+			return this.dealer.points;
 		},
 
 
