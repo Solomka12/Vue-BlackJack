d6ba7e9906436e6f6bf6c41eb7745476e8b03100
Solomka <solomahinigor@gmail.com>
2017-09-10 16:58:20 +0300

    Little refactoring
    Made a refactoring with points display. Add alerts with starting/ending
    game for now.

--- src/components/playingField.vue
+++ src/components/playingField.vue
@@ -3,7 +3,7 @@
 
 		<div class="dealer-area">
 			<transition name="fade">
-				<div v-show="setDealerPoints" class="dealer-points"><span class="points">{{setDealerPoints}}</span></div>
+				<div v-show="dealerPoints" class="dealer-points"><span class="points">{{ dealerPoints }}</span></div>
 			</transition>
 			<transition-group name="card-appear" tag="div" appear id="dealer-cards">
 				<div class="playing-card" v-bind:class="[card.name, card.suit]" v-for="(card, index) in dealer.hand" key="index"></div>
@@ -18,9 +18,31 @@
 				<button v-on:click="startDealerPlay" class="btn" v-bind:disabled="cantTakeMore" key="Enough">Enough</button>
 			</template>
 			<template v-else>
-				<input type="range" min="1" :max="player.cash" v-model="player.bet" id="rangePicker" step="1" key="range">
-				<input v-model.number="player.bet" type="number" name="money" min="1" :max="player.cash" step="1" value="10" key="bet">
-				<button v-on:click="giveOutCards()" class="btn" v-bind:disabled="player.bet < 1 || player.bet > player.cash" key="startHand">Bet</button>
+				<input 
+					type="range" 
+					min="1" 
+					:max="player.cash" 
+					v-model="player.bet" 
+					id="rangePicker" 
+					step="1" 
+					key="range">
+				<input 
+					v-model.number="player.bet" 
+					type="number" 
+					name="money" 
+					min="1" 
+					:max="player.cash" 
+					step="1" 
+					value="10" 
+					key="bet" 
+					v-bind:disabled="!player.cash">
+				<button 
+					v-on:click="giveOutCards()" 
+					class="btn" 
+					v-bind:disabled="player.bet < 1 || player.bet > player.cash" 
+					key="startHand"
+					>Bet
+				</button>
 			</template>
 			<div class="player-cash">{{ player.cash + '$'}}</div>
 			<!-- <button v-on:click="addCardTo(dealer)" class="btn" >Add Card To Dealer</button> -->
@@ -30,7 +52,7 @@
 
 		<div class="player-area">
 			<transition name="fade">
-				<div v-show="setPlayerPoints" class="player-points"><span class="points">{{player.points}}</span></div>
+				<div v-show="playerPoints" class="player-points"><span class="points">{{ playerPoints }}</span></div>
 			</transition>
 			<transition-group name="card-appear" tag="div" appear id="player-cards">
 				<div class="playing-card" v-bind:class="[card.name, card.suit]" v-for="(card, index) in player.hand" key="index"></div>
@@ -66,7 +88,7 @@
 
 			suits: ["spades", "clubs", "hearts", "diamonds"],
 
-			player:{ 
+			player: { 
 				hand: [],
 				cash: 1000,
 				points: 0,
@@ -78,6 +100,7 @@
 				points: 0,
 			},
 
+			playerName: '',
 			betMade: false,
 			cantTakeMore: false,
 
@@ -129,17 +152,16 @@
 		giveOutCards: function() {
 			this.player.cash -= this.player.bet;
 			this.betMade = true;
-			console.log('New game has been started');
 
 			this.addCardTo(this.dealer);
 			this.addCardTo(this.player);
 			this.addCardTo(this.player);
-			if (this.getPoints(this.player) === 21) this.startDealerPlay();
+			if (this.playerPoints === 21) this.startDealerPlay();
 		},
 
 		takeCard: function() {
 			this.addCardTo(this.player);
-			var points = this.getPoints(this.player);
+			var points = this.playerPoints;
 			if (points === 21) this.startDealerPlay();
 			else if (points > 21) { 
 				this.cantTakeMore = true;
@@ -160,14 +182,14 @@
 			/*----------Дилер добирает карты-----------*/
 			do {
 				this.addCardTo(this.dealer);
-			} while (this.getPoints(this.dealer) < 17);
+			} while (this.dealerPoints < 17);
 
 			this.summarizing();
 		},
 		
 		/*----------Сравниваются очки дилера и игрока-----------*/
 		summarizing: function() {
-			if (this.getPoints(this.dealer) > 21 || this.getPoints(this.player) > this.getPoints(this.dealer)) {
+			if (this.dealerPoints > 21 || this.playerPoints > this.dealerPoints) {
 				this.player.cash += this.player.bet * 2;
 				// console.info('You won ' + this.player.bet * 2 + '$');
 				this.$notify({
@@ -176,7 +198,7 @@
 					type: 'my-success',
 				});
 				this.history.unshift(this.wrightToHistory('win'));
-			} else if (this.getPoints(this.player) === this.getPoints(this.dealer)) {
+			} else if (this.playerPoints === this.dealerPoints) {
 				this.player.cash += +this.player.bet;
 				// console.warn('Draw');
 				this.$notify({
@@ -208,26 +230,13 @@
 				})(),
 				status: status,
 				player: {
-					points: this.getPoints(this.player),
+					points: this.playerPoints,
 				},
 				dealer: {
-					points: this.getPoints(this.dealer),
+					points: this.dealerPoints,
 				}
 			};
 		},
-
-			// var points = this.getPoints(this.dealer);
-			// var i = 0;
-			// do {
-			// 	setTimeout(function(that) {
-			// 		return function() {
-			// 			console.log(that);
-			// 			that.addCardTo(that.dealer);
-			// 		}; 
-			// 	}(this), 1000*i);
-			// 	points = this.getPoints(this.dealer);
-			// 	i++;
-			// }	while (points < 17);
 
 		getPoints: function(person) {
 			var ace = 0;
@@ -248,9 +257,7 @@
 				return sum + currentCard.value;
 			}, 0);
 		},
-
-
-},
+	},
 
 // directives: {
 // 	changeCardsMargin: {
@@ -265,22 +272,35 @@
 // 	},
 // },
 
-computed: {
+	computed: {
 
-	setPlayerPoints: function() {
-			// if (this.getPoints(this.player) < 21 && this.getPoints(this.player) > this.getPoints(this.dealer)) console.info('You won!');
-			// else if (this.getPoints(this.player) > 21) console.warn('Overfull! You lose...');
-			// else console.warn('You lose(');
-			this.player.points = this.getPoints(this.player);
-			// console.log(this.player.points);
-			return this.player.points;
-		},
-		
-		setDealerPoints: function() {
-			this.dealer.points = this.getPoints(this.dealer);
-			return this.dealer.points;
+		playerPoints: function() {
+			return this.getPoints(this.player)
 		},
 
+		dealerPoints: function() {
+			return this.getPoints(this.dealer)
+		},
+
+	},
+
+	watch: {
+		// watchCashAmount: function(val) {
+		// 	console.log('cash: ' + val)
+		// 	if( val < 1 && !this.betMade ) confirm('You run out of money. Try again?') ? this.player.cash = 1000 : alert('This is the end...');
+		// },
+
+		player: {
+			handler: function (val) {
+				if( val.cash < 1 && !this.betMade ) confirm('You run out of money. Try again?') ? val.cash = 1000 : alert('This is the end...');
+			},
+			deep: true
+		},
+	},
+
+	created: function() {
+		this.playerName = prompt('Enter your name: ');
+		console.log('Hi, ' + this.playerName);
 	}
 
 }
